37dd061fddff941183c799970e068f4006d47534
Charles Njoroge <charles.maina@student.moringaschool.com>
2023-05-29 21:24:57 +0300

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -2,17 +2,17 @@ spicy_foods = [
     {
         "name": "Green Curry",
         "cuisine": "Thai",
-        "heat_level": 9,
+        "heat_level": 8,
     },
     {
         "name": "Buffalo Wings",
         "cuisine": "American",
-        "heat_level": 3,
+        "heat_level": 2,
     },
     {
         "name": "Mapo Tofu",
         "cuisine": "Sichuan",
-        "heat_level": 6,
+        "heat_level": 5,
     },
 ]
 
